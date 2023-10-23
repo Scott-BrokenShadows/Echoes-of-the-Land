08399f332d5d8f09eb4ce161f0da6fb0958296eb
Andy <48786962+Axylum@users.noreply.github.com>
2023-10-23 15:09:59 +0800

    +8 log on Match 3
    Numerous compile errors likely because of a function and variable input

--- Assets/Andy/Match 3 sample files/Scripts/Match3.cs
+++ Assets/Andy/Match 3 sample files/Scripts/Match3.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Match3 : MonoBehaviour
 {
     public ArrayLayout boardLayout;
+    public Sprite[] pieces;
     int width = 9;
     int height = 14;
     Node[,] board;
@@ -32,10 +33,126 @@ public class Match3 : MonoBehaviour
         {
             for(int x = 0; x < width; x++)
             {
-                board[x,y] = new Node(-1, new Point(x,y));
+                board[x,y] = new Node((boardLayout.rows[y].row[x]) ? - 1 : fillPiece(), new Point(x,y));
             }
         }   
     }
+
+    void VerifyBoard()
+    {
+        board = new Node[width, height];
+        for(int y = 0; y < height; y++)
+        {
+            for(int x = 0; x < width; x++)
+            {
+                Point p = new Point(x,y);
+                int val = getValueAtPoint(p);
+                if(val <= 0) continue;
+
+
+            }
+        } 
+    }
+
+    List<Point> isConnected(Point p, bool main)
+    {
+        List<Point> connected = new List<Point>();
+        int val = getValueAtPoint(p);
+        Point[] directions = 
+        {
+            Point.up,
+            Point.right,
+            Point.down,
+            Point.left
+        };
+
+        foreach(Point dir in directions) //Checking if there are 2 or more of same shapes in the directions
+        {
+            List<Point> line = new List<Point>();
+
+            int same = 0;
+            for (int i = 1; i < 3; i++)
+            {
+                Point check = Point.add(p, Point.mult(dir, i));
+                if(getValueAtPoint(check) == val)
+                {
+                    line.Add(check);
+                    same++;
+                }
+            }
+
+            if(same > 1) //if there are more than 1 of the same shape in the direction then it's a match
+                AddPoints(ref connected, line); //Add these points to the overarching connected list
+        }
+
+        for(int i = 0; i < 2; i++) //Checking if we are in the middle of two of the same shapes, aka the middle
+        {
+            List<Point> line = new List<Point>();
+
+            int same = 0;
+            Point[] check = { Point.add(p, directions[i]), Point.add(p, directions[i+2]) };
+            foreach (Point next in check) //Check both sides of the piece if they're same value, add them to the list
+            {
+                if(getValueAtPoint(next) == val)
+                {
+                    line.Add(p);
+                    same++;
+                }
+            }
+
+            if(same > 1)
+                AddPoints(ref connected, line);
+        }
+
+        for(int i = 0; i < 4; i++) //check for a 2x2
+        {
+            List<Point> square = new List<Point>();
+
+            int same = 0;
+            int next = i + 1;
+            if(next >= 4)
+                next -= 4;
+
+            Point[] check = { Point.add(p, directions[i]), Point.add(p, directions[next]), Point.add(p, Point.add(directions[i], directions[next])) };
+            foreach (Point pnt in check) //Check all sides of the piece if they're same value, add them to the list
+            {
+                if(getValueAtPoint(pnt) == val)
+                {
+                    square.Add(p);
+                    same++;
+                }
+            }
+
+            if(same > 2)
+                AddPoints(ref connected, square);
+        }
+
+        if(main)
+        {
+            for(int i = 0; i < connected.Count; i++)
+            {
+                AddPoints(ref connected, isConnected(connected[i], false));
+            }
+        }
+    }
+
+    void AddPoints(ref List<Point> points, List<Point> add)
+    {
+
+    }
+
+    int getValueAtPoint(Point p)
+    {
+        return  board[p.x, p.y].value;
+    }
+
+    int fillPiece ()
+    {
+        int val = 1;
+        val = (random.Next(0,100) / (100 / pieces.Length)) + 1;
+        return val;
+    }
+
     // Update is called once per frame
     void Update()
     {
