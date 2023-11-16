41fd7093659c103f6eb4a60892f1b5c8f483f7ad
Scott <1026018@student.sae.edu.au>
2023-11-16 19:32:01 +0800

    Highlight issue
    trying to fix highlight issue, but it's just not working

--- Assets/Daniel/Scripts/InventoryHighlight.cs
+++ Assets/Daniel/Scripts/InventoryHighlight.cs
@@ -22,7 +22,7 @@ public class InventoryHighlight : MonoBehaviour
     public void SetPosition(ItemGrid targetGrid, InventoryItem targetItem)
     {
         Vector2 pos = targetGrid.CalculatePositionOnGrid(targetItem, targetItem.onGridPositionX, targetItem.onGridPositionY);
-        highlighter.position = pos;
+        highlighter.localPosition = pos;
     }
 
     public void SetParent(ItemGrid targetGrid)
@@ -33,20 +33,9 @@ public class InventoryHighlight : MonoBehaviour
 
     public void SetPosition(ItemGrid targetGrid, InventoryItem targetItem, int posX, int posY)
     {
-        //Vector2 pos = targetGrid.CalculatePositionOnGrid(targetItem, posX, posY);
-        //highlighter.localPosition = pos;
-
         Vector2 pos = targetGrid.CalculatePositionOnGrid(targetItem, posX, posY);
-        RectTransform targetRectTransform = targetItem.GetComponent<RectTransform>();
-
-        // Convert the position from local space to screen space
-        Vector2 screenPos = RectTransformUtility.WorldToScreenPoint(null, pos);
-
-        // Convert the screen position to local position within the canvas
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(highlighter.parent as RectTransform, screenPos, null, out pos);
-
-        // Set the local position of the highlighter
         highlighter.localPosition = pos;
+
     }
 
 }
